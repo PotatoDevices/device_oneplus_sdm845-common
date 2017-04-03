d4f10ef4f49a219584052f856057faf32f8f869c
Amit Shekhar <ashekhar@codeaurora.org>
2017-04-03 15:49:55 -0700

    mediacodec: Allow select and eventfd syscalls
    Add pselect6 and eventfd2 syscalls
    Includes following:
    mediacodec: allow sendto and recvfrom syscalls
    mediacodec: allow lseek syscall
    mediaextractor: allow pread64 and readlinkat syscalls
    mediacodec: allow getdents64, sysinfo and getcwd
    
    Vendor extended policy files are separated from the aosp
    and will now reside in /vendor
    
    Change-Id: If59d04b36d744efb74209ae36e76dd16cb18f606
    CRs-Fixed: 2027600
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Raj Jadeja <rajsjadeja4@gmail.com>

--- common.mk
+++ common.mk
@@ -262,6 +262,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     power.qcom:64
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+
 # Media
 PRODUCT_PACKAGES += \
     libmediaplayerservice \
