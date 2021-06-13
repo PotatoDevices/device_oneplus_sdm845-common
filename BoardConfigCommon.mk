6afa6bef8bca9d40d116fbb281882d2d4689c02a
Raj Jadeja <rajsjadeja4@gmail.com>
2021-06-13 15:49:15 +0000

    Revert "sdm845-common: Update kernel defconfig"
    This reverts commit e13d1f6feef8f4e9e231efbf65d20b3f056ab0d0.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -64,8 +64,7 @@ TARGET_KERNEL_SOURCE := kernel/oneplus/sdm845
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_USE_LATEST_CLANG := true
-TARGET_KERNEL_CONFIG := enchilada_defconfig
-TARGET_KERNEL_ADDITIONAL_FLAGS := AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
+TARGET_KERNEL_CONFIG := syberia_defconfig
 
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
